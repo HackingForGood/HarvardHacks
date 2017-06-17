ffab5b8f23eead6b9d2e68fa2a784ad2e222a05d
hljames <haileyjames@college.harvard.edu>
2017-06-17 11:52:40 -0400

    basic config functionality

--- static/js/teacher_config.js
+++ static/js/teacher_config.js
@@ -1,2 +1,9 @@
 window.onload = function() {
 }
+
+function add() {
+  console.log("clicked");
+  var newclass = document.getElementById("newclass").value;
+  var classes = document.getElementById("classes");
+  classes.options[classes.options.length] = new Option(newclass, newclass);
+}
