59dce3d22ab51c51dbf7c64d1ae95274efb0a919
Brian Yu <brianyu28@gmail.com>
2017-06-17 14:58:56 -0400

    Fix socketio run issues

--- application.py
+++ application.py
@@ -49,4 +49,4 @@ def student_yes():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port)
