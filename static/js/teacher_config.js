034184b63ced22e1d05a5f6f8b06317c93382253
Joey C <joeyc@Joeys-iMac.local>
2017-06-17 17:12:09 -0400

    add and remove classes working

--- static/js/teacher_config.js
+++ static/js/teacher_config.js
@@ -12,7 +12,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     // console.log(user);
     function addClass (className) {
       // '/user-posts/' + uid + '/' + newPostKey
-      if (className != undefined) {
+      if (className !== "") {
       db.ref('classes/' + className).set({
         className: className,
         teacher: uid,
@@ -20,15 +20,62 @@ firebase.auth().onAuthStateChanged(function(user) {
     }
     };
 
-    function addStudent (studentName, studentEmail) {
+    function removeClass (className) {
       // '/user-posts/' + uid + '/' + newPostKey
-      if ((studentEmail != undefined) && (studentName != undefined)) {
-      db.ref('students/' + studentName).set({
-        name: studentName,
-        email: studentEmail,
-      })
+      if (className !== "") {
+      db.ref('classes/' + className).remove();
     }
     };
+
+
+    function addStudent (studentName, studentEmail, currentName) {
+    //   if ((studentEmail !== "") && (studentName !== "")) {
+    //     if ((db.ref(‘students/’ + studentEmail)) !== "") {
+    //   db.ref(‘students/’ + studentName).set({
+    //     name: studentName,
+    //     email: studentEmail,
+    //   });
+    // };
+      db.ref("classes/" + currentName + "/students").set({
+        name: studentName,
+        email: studentEmail,
+        time: 0,
+    });
+
+    };
+
+    // function removeClass (className) {
+    //   // '/user-posts/' + uid + '/' + newPostKey
+    //   if (className !== "") {
+    //   db.ref('classes/' + className).remove();
+    //   }
+    // };
+    //
+    // $(".removeClass").val().change(function() {
+    //     var cName = $(".removeClass").val()
+    //     console.log('HERE, removing: ' + cName);
+    //     removeClass(cName);
+    // })
+
+
+    // function addStudent (studentName, studentEmail, className) {
+    //   // '/user-posts/' + uid + '/' + newPostKey
+    //   if ((studentEmail != undefined) && (studentName != undefined)) {
+    //   db.ref('students/' + studentName).set({
+    //     name: studentName,
+    //     email: studentEmail,
+    //   })
+    // }
+    // };
+
+    $( ".removeClass" ).click(function() {
+        console.log("remove clicked");
+        console.log($(this).siblings("select").find(":selected"));
+        var className = $( this ).siblings( "select").find(":selected").text();
+        console.log('CLASSNAME + '+ className);
+        removeClass(className)
+      });
+
 
     function writeTeacherData(username, email) {
       db.ref('teachers/' + uid).set({
@@ -49,7 +96,8 @@ firebase.auth().onAuthStateChanged(function(user) {
           console.log("addStudent clicked");
           var studentEmail = $( "#studentEmail" ).val();
           var studentName = $( "#studentName" ).val();
-          addStudent (studentName, studentEmail)
+          var className = $("#classes").find(":selected").val();
+          addStudent (studentName, studentEmail, className)
         });
       writeTeacherData("hailey", "hjames");
       console.log("ran");
@@ -83,11 +131,6 @@ firebase.auth().onAuthStateChanged(function(user) {
 //     $( this ).siblings( "select").find(":selected").remove();
 //   });
 //
-// $( ".removeStudents" ).click(function() {
-//     console.log("remove clicked");
-//     console.log($(this).siblings("select").find(":selected"));
-//     $( this ).siblings( "select").find(":selected").remove();
-//   });
 
 
 
